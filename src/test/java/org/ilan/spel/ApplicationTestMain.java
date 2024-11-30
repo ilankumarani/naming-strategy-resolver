e08e9e30d20c40e3c4aeb0ab18c28bbb4f06db29
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-11-30 21:28:19 +0000

    Resolve Spring Expression Language from application.yml

---
+++ src/test/java/org/ilan/spel/ApplicationTestMain.java
@@ -0,0 +1,12 @@
+package org.ilan.spel;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationTestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationTestMain.class, args);
+    }
+}
