e7b5236ba425e4510ea871769b920138fbd5415b
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2024-12-01 23:25:08 +0000

    Resolve Spring Expression Language from application.yml

--- src/main/java/org/ilan/constant/NamingStrategyConstant.java
+++ src/main/java/org/ilan/constant/NamingStrategyConstant.java
@@ -3,7 +3,7 @@ package org.ilan.constant;
 public class NamingStrategyConstant {
 
     public static final String SPRING_PHYSICAL_NAMING_STRATEGY = "spring.jpa.hibernate.naming.physical-strategy";
-    public static final String PHYSICAL_NAMING_STRATEGY_ENABLED = "naming.strategy.physical_naming_strategy.enabled";
+    public static final String PHYSICAL_NAMING_STRATEGY_ENABLED = "naming.physical-strategy.enabled";
 
     private NamingStrategyConstant() {
 
