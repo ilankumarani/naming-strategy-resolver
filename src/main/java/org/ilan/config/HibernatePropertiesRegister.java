a8f35a60ef2704d17eea83d8502655c9967675a7
Ilannkumaran Ilangovan <kumaranilan@yahoo.com>
2025-06-14 12:49:59 +0100

    Release new version 2.0.0

--- src/main/java/org/ilan/config/HibernatePropertiesRegister.java
+++ src/main/java/org/ilan/config/HibernatePropertiesRegister.java
@@ -16,8 +16,7 @@ import org.springframework.core.env.PropertiesPropertySource;
 import java.util.Objects;
 import java.util.Properties;
 
-import static org.ilan.constant.NamingStrategyConstant.PHYSICAL_NAMING_STRATEGY_ENABLED;
-import static org.ilan.constant.NamingStrategyConstant.SPRING_PHYSICAL_NAMING_STRATEGY;
+import static org.ilan.constant.NamingStrategyConstant.*;
 
 /**
  * Register the Naming strategy when Spring creates Bean of entityManager
@@ -42,5 +41,18 @@ public class HibernatePropertiesRegister implements ApplicationListener<Applicat
 
         ConfigurableListableBeanFactory configurableListableBeanFactory = ((AnnotationConfigApplicationContext)applicationContext).getBeanFactory();
         CarrierSpelResolverProvider.setConfigurableListableBeanFactory(configurableListableBeanFactory);
+
+        String isEnabled = environment.getProperty(PHYSICAL_NAMING_STRATEGY_ENABLED);
+
+        //No flag provided
+        //Flag provide but empty value
+        //Flag provide but value with space
+        //Flag provide and the value is TRUE
+        if (Objects.isNull(isEnabled) || isEnabled.isEmpty() || (Objects.nonNull(isEnabled) && isEnabled.trim().isEmpty()) || (Objects.nonNull(isEnabled) && !isEnabled.trim().isEmpty() && Boolean.valueOf(isEnabled.trim()) == Boolean.TRUE)) {
+            Properties props = new Properties();
+            props.put(HIBERNATE_PHYSICAL_NAMING_STRATEGY, CustomPhysicalNamingStrategy.CLASS_NAME);
+            final String NAMING_STRATEGY_PROPERTY = "physicalNamingProperty";
+            environment.getPropertySources().addFirst(new PropertiesPropertySource(NAMING_STRATEGY_PROPERTY, props));
+        }
     }
 }
--- src/main/java/org/ilan/constant/NamingStrategyConstant.java
+++ src/main/java/org/ilan/constant/NamingStrategyConstant.java
@@ -12,7 +12,7 @@ public class NamingStrategyConstant {
     public static final String SPRING_PHYSICAL_NAMING_STRATEGY = "spring.jpa.hibernate.naming.physical-strategy";
 
     /**
-     * Spring property registering the naming-strategy
+     * Hibernate property registering the naming-strategy
      */
     public static final String HIBERNATE_PHYSICAL_NAMING_STRATEGY = "spring.jpa.properties.hibernate.physical_naming_strategy";
 
